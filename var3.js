52395318918d2aed84410d994670c604721c3e83
Games_BUSH_ <129669005+GamesBUSH@users.noreply.github.com>
2023-04-09 16:48:43 +0300

    Create var3.js

---
+++ var3.js
@@ -0,0 +1,9 @@
+var animals = ['Кошка','Собака','Корова','Лошадь','Овца','Гусь'];
+
+alert('Исходный список: ' + animals);
+
+var animals2 = animals.splice(2, 3);
+
+alert('Измененный список: ' + animals);
+
+alert('Измененный список: ' + animals2);
