6aa1c60a8e2469d3fc616c2ad26a85440403b33d
Games_BUSH_ <129669005+GamesBUSH@users.noreply.github.com>
2023-04-09 17:16:05 +0300

    Create animals.js

---
+++ animals.js
@@ -0,0 +1,25 @@
+var animals = ['Кошка','Собака','Корова','Лошадь','Овца','Гусь'];
+
+alert('Исходный список: ' + animals);
+
+var animals2 = animals.splice(2, 3);
+
+alert('Измененный список: ' + animals);
+
+alert('Измененный список: ' + animals2);
+
+name = prompt("Введите любое животное:", ",Шиншила ");
+
+alert  ("Ваш список: "+animals+""+name+ "");
+
+animals.unshift(animals2)
+
+name = prompt('Введите любое животное для удаления:э' - 1);
+
+alert.splice(num, 1);
+
+alert(animals);
+
+animals.shift();
+
+alert(animals);
