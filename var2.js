1ab1a960d0df5c7118d5d79d1aa5ccbab0a5eb21
Games_BUSH_ <129669005+GamesBUSH@users.noreply.github.com>
2023-04-09 16:23:13 +0300

    Create var2.js

---
+++ var2.js
@@ -0,0 +1,19 @@
+var animals = ['Кошка','Собака','Корова'];
+
+//alert(animals);
+
+//alert(animals[1]);
+
+animals.push('Лошадь');
+
+animals.unshift('Гусь');
+
+alert(animals);
+
+animals.pop();
+
+//alert(animals);
+
+animals.shift();
+
+alert(animals);
