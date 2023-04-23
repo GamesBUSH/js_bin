54ebbfa5cf14502c4ef0921c5b0e73ce73c9e94b
Games_BUSH_ <129669005+GamesBUSH@users.noreply.github.com>
2023-04-23 17:22:17 +0300

    Create cube.js

---
+++ cube.js
@@ -0,0 +1,8 @@
+function cube(num) {
+    function num * num; {
+      return num *4;
+  }
+  return square() * num;
+}
+ number = prompt ('Введите число:');
+alert('Число в кубе: ' + cube(numder));
